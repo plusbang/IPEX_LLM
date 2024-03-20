eca3ed2c8777dc6e628bfa0604440792e9e755b8
plusbang <binbin1.deng@intel.com>
2024-03-20 20:18:53 +0800

    test qwen1.5

--- python/llm/dev/benchmark/all-in-one/run-deepspeed-arc.sh
+++ python/llm/dev/benchmark/all-in-one/run-deepspeed-arc.sh
@@ -14,3 +14,7 @@ export SYCL_PI_LEVEL_ZERO_USE_IMMEDIATE_COMMANDLISTS=2
 export TORCH_LLM_ALLREDUCE=0 # Different from PVC
 
 mpirun -np $NUM_GPUS --prepend-rank python run.py
+# export CCL_DG2_ALLREDUCE=1 # For internal CCL
+
+# CCL_ROOT=/home/arda/binbin/perf-optimize/oneccl-from-jianwei/rollback-stable-version/1ccl_dg2_allreduce_20240308  LD_LIBRARY_PATH=/home/arda/binbin/perf-optimize/oneccl-from-jianwei/rollback-stable-version/1ccl_dg2_allreduce_20240308/src:/home/arda/binbin/perf-optimize/oneccl-from-jianwei/rollback-stable-version/1ccl_dg2_allreduce_20240308/deps/mpi/lib:/opt/intel/oneapi/2024.0/lib \
+#     mpirun -np $NUM_GPUS --prepend-rank python run.py
